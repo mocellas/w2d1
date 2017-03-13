c6207e5b8e4a85ae0a4b5bbf01c63f46a43af730
mocellas <fernando.mocellin@gmail.com>
2017-03-13 18:46:28 +0000

    Code running as expected.

---
+++ getAndPrintHtmlChunks.js
@@ -0,0 +1,34 @@
+var https = require('https');
+
+var getAndPrintHTMLChunks = function() {
+
+    var requestOptions = {
+      host: 'sytantris.github.io',
+      path: '/http-examples/step1.html'
+    };
+
+    /* Add your code here */
+
+    // notice that https.get takes a callback with one parameter -
+  // response, which is a Stream that represents the HTTP response
+  https.get(requestOptions, function (response) {
+    console.log(response.statusCode);
+
+    // set encoding of received data to UTF-8
+    response.setEncoding('utf8');
+
+    // the callback is invoked when a `data` chunk is received
+    response.on('data', function (data) {
+      console.log(1);
+      console.log('Chunk Received. Length:' + "\n", data.length);
+    });
+
+    // the callback is invoked when all of the data has been received
+    // (the `end` of the stream)
+    response.on('end', function() {
+      console.log('Response stream complete.');
+    });
+  });
+}
+
+getAndPrintHTMLChunks();
