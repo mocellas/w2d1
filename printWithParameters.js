62542cccf010812db21d72afe8eb60dc4679e232
mocellas <fernando.mocellin@gmail.com>
2017-03-13 21:46:02 +0000

    Change variable getParameters by requestOptions

--- printWithParameters.js
+++ printWithParameters.js
@@ -1,32 +1,33 @@
-var https = require('https');
+  var https = require('https');
 
-function getAndPrintHTML (options) {
+  function getAndPrintHTML (options) {
 
-/* Add your code here */
+  /* Add your code here */
 
 
-var requestOptions = {
-  host: 'sytantris.github.io',
-  path: '/http-examples/step3.html'
-};
-https.get(requestOptions, function (response) {
-  console.log("What hell is going on here:", response)
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step3.html'
+  };
 
-  response.setEncoding('utf8');
+  https.get(requestOptions, function (response) {
+    console.log("What hell is going on here:", response)
 
-    getParameters = [];
-    response.on('data', function (data) {
-    getParameters.push(data);
-    console.log(getParameters);
+    response.setEncoding('utf8');
 
-  });
+      requestOptions = [];
+      response.on('data', function (data) {
+      requestOptions.push(data);
+      console.log(requestOptions);
 
-  response.on('end', function() {
-    console.log('Data Recived:', getParameters.join(""));
-    console.log('Response stream complete.');
-  });
+    });
 
- });
-}
+    response.on('end', function() {
+      console.log('Data Recived:', requestOptions.join(""));
+      console.log('Response stream complete.');
+    });
 
-getAndPrintHTML();
+   });
+  }
+
+  getAndPrintHTML();
